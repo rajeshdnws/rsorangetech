b37e726b2550a53f715522b1baf9ac4127404574
suman <sumandnw@gmaail.com>
2025-04-19 13:48:53 +0530

    push all code

---
+++ wp-content/themes/rsorangetech/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header>
+    <div class="container">
+        <h1><a href="<?php echo home_url(); ?>">rsorangetech</a></h1>
+        <nav><?php wp_nav_menu(array('theme_location' => 'primary')); ?></nav>
+    </div>
+</header>
